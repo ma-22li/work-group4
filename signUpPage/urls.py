ed724a34f1581f72beaaf9f7e36e82f865bd91e6
YazeedAlsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2022-08-03 04:34:47 +0300

    Delete signUpPage directory

--- signUpPage/urls.py
+++
@@ -1,8 +0,0 @@
-from django import views
-from django.http import HttpResponse
-from django.urls import URLPattern, path
-from . import views
-
-urlpatterns = [
-    path('', views.signUp, name="home"),
-]
