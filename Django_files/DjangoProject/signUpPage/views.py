1c0bdcf0cb471833e33f3dfce240bb4cf13acae7
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-08-04 10:28:45 +0300

    function (signup)

--- Django_files/DjangoProject/signUpPage/views.py
+++ Django_files/DjangoProject/signUpPage/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
 # Create your views here.
-def signUp(request):
-    return HttpResponse('Hello World')
+def signup(request):
+    msg=None
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            msg =' user created'
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form, 'msg':msg})
