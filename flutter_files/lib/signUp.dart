ced82dc5d31ffbed05d87c0d4957cf22782b5a45
YazeedAlsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2022-08-04 15:34:49 +0300

    Create signUp.dart

---
+++ flutter_files/lib/signUp.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+class signUpScreen extends StatefulWidget{
+  const signUpScreen({ Key? key }) : super(key: key);
+  
+  @override
+  State<signUpScreen> createState() => _signUpScreenState();
+}
+
+
+class _signUpScreen extends State<signUpScreen> {
+  
+  
+  void signUp(){
+    
+  }
+  
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: const Color(0xFFFFE306));
+  }
+}
