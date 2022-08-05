57af237550023af4620f6177b6ecb77c4339d5e6
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-08-05 13:08:26 +0300

    edit url

--- Django_files/DjangoProject/signUpPage/urls.py
+++ Django_files/DjangoProject/signUpPage/urls.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.urls import URLPattern, path
 from . import views
 
-urlpatterns = [
-    path('', views.signUp, name="home"),
+urlpatterns = [   
+    path('admin/', admin.site.urls),
+    path('',views.home,name='home'),
+    path('signup/',views.signup, name='signup')
 ]
