8697ff8d7dd87532eabab929d8e791ec70ae2781
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-08-05 13:25:59 +0300

    Update settings.py

--- Django_files/DjangoProject/settings.py
+++ Django_files/DjangoProject/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'quran_db', 
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE':'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3',
     }
 }
 
