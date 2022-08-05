7af662cd294f65fb8c4c9649f33bd4580ca6fe20
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-08-05 13:06:00 +0300

    add templates

--- Django_files/DjangoProject/signUpPage/views.py
+++ Django_files/DjangoProject/signUpPage/views.py
@@ -1,16 +1,25 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import  authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
 # Create your views here.
 def signup(request):
-    msg=None
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method=='POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            msg =' user created'
-            return redirect('home')
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaand_data.get('password1')
+            user = authenticate(username=username,password=password)
+
+            return redirect('/')
+        else:
+            return render(request,'signup.html',{'form':form})
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form, 'msg':msg})
+        return render (request,'signup.html',{'form':form})
+
+def home(req):
+    return render(req,'home.html')
