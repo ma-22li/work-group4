1e2f4bd3dab723bda808395313bef773b3bb0020
YazeedAlsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2022-08-07 16:24:17 +0300

    Update main.dart

--- flutter_files/lib/main.dart
+++ flutter_files/lib/main.dart
@@ -1,50 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:workgroup4_flutter/Screens/signUp.dart';
 
-void main(){
-  //TODO link the userName and Password and email got from user input to the below variables 
-  //String UserName;
-  //String Password;
-  //String Email;
-    
-  validCheck str = new validCheck(UserName,Password,Email);
-  print(str.ValidInput());
+void main() {
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
-
-class validCheck {
-  
-  
-  String UserName;
-  String Password;
-  String Email;
-
-  validCheck(this.UserName,this.Email,this.Password);
-
-  bool isNumeric(String str) {
-    bool found = str.contains(new RegExp(r'[0-9]'));
-    return found;
-} 
-  
-  
-  String ValidInput(){
-  
-  if(UserName.isEmpty){ 
-    return "User Name is empty you should fill it";
-  }
-  if(Password.isEmpty){ 
-    return "Password is empty you should fill it";
-  }
-  if(Email.isEmpty){ 
-    return "Email is empty you should fill it";
-  }
-  if(isNumeric(UserName)){ 
-    return "User Name must not contain any number!";
-  }
-  bool emailValid = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(Email);
-  if(!emailValid){
-    return "Email form is not correct!";
-  }
-  return "success";
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Home',
+      home: SignUpScreen(),
+    );
   }
 }
