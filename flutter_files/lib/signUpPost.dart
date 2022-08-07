82a406bb63c0fd9ce56ef604b41625b080ca9332
YazeedAlsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2022-08-07 16:25:20 +0300

    Create signUpPost.dart

---
+++ flutter_files/lib/signUpPost.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+
+class SignUpPost {
+  String urL = "http://localhost:8000/register";
+  String resp;
+
+  var _myHeader = {
+    'Content-type': 'application/json',
+  };
+
+  Future<void> signUpPost(
+      String userName, String email, String password) async {
+    Uri url = Uri.parse(urL);
+
+    var _myBody = {
+      'username': userName,
+      'email': email,
+      'password': password,
+    };
+
+    var response =
+        await http.post(url, headers: _myHeader, body: jsonEncode(_myBody));
+    resp = response.body;
+    print(resp);
+  }
+}
