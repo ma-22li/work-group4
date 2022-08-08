42313b739098e7f8514b3ddded78354b44707c7e
YazeedAlsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2022-08-08 17:16:22 +0300

    Add files via upload

---
+++ Django_files/DjangoProject/api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+
+#Serializer to Get User Details using Django Token Authentication
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    fields = ["id", "username"]
+#Serializer to Register User
+class RegisterSerializer(serializers.ModelSerializer):
+  email = serializers.EmailField(
+    required=True,
+    validators=[UniqueValidator(queryset=User.objects.all())]
+  )
+  password = serializers.CharField(
+    write_only=True, required=True, validators=[validate_password])
+  class Meta:
+    model = User
+    fields = ('username','password','email',)
+  def create(self, validated_data):
+    user = User.objects.create(
+      username=validated_data['username'],
+      email=validated_data['email'],
+    )
+    user.set_password(validated_data['password'])
+    user.save()
+    return user
