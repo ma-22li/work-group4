7c7860bac5d7ad2bcc40c2c58f4ce85c5ccf0847
salwa alqurashi <92172130+Salwaq@users.noreply.github.com>
2022-08-11 09:49:28 +0300

    clean file .

--- Django_files/DjangoProject/c2/User/models.py
+++
@@ -1,8 +0,0 @@
-import email
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    name= models.CharField(max_length=100)
-    email= models.EmailField()
-    password= models.CharField(max_length=200)
--- Django_files/DjangoProject/c2/User/views.py
+++
@@ -1,7 +0,0 @@
-import http
-from http.client import HTTPResponse
-
-
-# Create your views here.
-def index(response):
-    return HTTPResponse('hello database')
